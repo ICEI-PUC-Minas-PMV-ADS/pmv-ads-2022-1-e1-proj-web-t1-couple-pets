153bc9cac24b6d0ab3b85797f15e23bb84a399a0
sioliv <silvinha.olivert@gmail.com>
2022-06-06 19:56:27 -0300

    Atualizações

--- src/JS/login.js
+++ src/JS/login.js
@@ -1,14 +1,14 @@
 function logar() {
 
-    //Desafio: Armazenar em localStorage já que não vamos utilizar BD
+    // Armazenar em localStorage já que não vamos utilizar BD
     
     var email = document.getElementById("email").value;
     var password = document.getElementById("pwd").value;
 
     if (email == "pedro@gmail.com" && password == "admin") {
-        location.href = "/src/HTML/Perfil.html";
+        location.href = "/src/HTML/info-joao.html";
     } else if (email == "laura@gmail.com" && password == "laura") {
-        location.href = "/src/HTML/Perfil.html";
+        location.href = "/src/HTML/info-laura.html";
     } else {
         alert('Usuário inexistente ou senha incorretos');
     
