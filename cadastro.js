9bf4440412a4b495bbf4db1b8cb8ca705117a124
Rafael Souza <rafael.souza54@hotmail.com>
2022-06-09 23:15:33 -0300

    alteraçoes de navegação e responisvidade

---
+++ cadastro.js
@@ -0,0 +1,20 @@
+
+function cadastro() {
+
+    // é melhor armazenar em localStorage e melhorar mais a logica
+   
+    var EmailCadastro = document.getElementById("myEmail").value;
+    var myPassword = document.getElementById("myPassword").value;
+
+    if(EmailCadastro == "pedro@gmail.com" && myPassword == "admin"){
+        location.href = "/src/HTML/Perfil.html";
+
+    }else if (EmailCadastro == "laura@gmail.com" && myPassword == "laura"){
+        location.href = "/src/HTML/Perfil.html";
+
+    }else{
+        alert('insira um email e senha para cadastro');
+
+    }
+        
+}
