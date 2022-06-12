c7ebbf8f4a9de86d1538c910cced0dfd02937297
Rafael Souza <rafael.souza54@hotmail.com>
2022-06-12 19:20:53 -0300

    alteração de nomes HTML

--- src/JS/perfil.js
+++ src/JS/perfil.js
@@ -1,7 +1,33 @@
+document.querySelector('#imgInp').addEventListener("change", function () {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", () => {
+        localStorage.setItem("imagem-recente", reader.result)
+    });
+
+    reader.readAsDataURL(this.files[0]);
+})
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const dadoUrlImagemRecente = localStorage.getItem("imagem-recente");
+    if (dadoUrlImagemRecente) {
+        document.querySelector("#blah").setAttribute("src", dadoUrlImagemRecente);
+    }
+});
+
+
+imgInp.onchange = evt => {
+    const [file] = imgInp.files
+    if (file) {
+        blah.src = URL.createObjectURL(file)
+    }
+}
+
 function Salvar() {
     localStorage.setItem("nome", document.getElementById("nome").value);
     localStorage.setItem("dtaNasc", document.getElementById("dtaNasc").value);
-    localStorage.setItem("gender", document.getElementsByName("gender").value);
+    //localStorage.setItem("gender", document.getElementsByName("gender", checked).value);
     localStorage.setItem("endereco", document.getElementById("endereco").value);
     localStorage.setItem("tel", document.getElementById("tel").value);
     localStorage.setItem("cidade", document.getElementById("cidade").value);
@@ -15,7 +41,3 @@ function Carregar() {
     document.getElementById("tel").value = localStorage.getItem("tel");
     document.getElementById("cidade").value = localStorage.getItem("cidade");
 }
-
-
- 
-  
