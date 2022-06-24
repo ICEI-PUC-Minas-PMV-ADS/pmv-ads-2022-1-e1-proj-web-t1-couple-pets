89e0d5fe6548958b49445e94e90c54f88ea6326b
Rafael Souza <rafael.souza54@hotmail.com>
2022-06-23 21:58:59 -0300

    alterações de script bem-bindo

--- src/JS/app.js
+++ src/JS/app.js
@@ -88,3 +88,85 @@ var db = JSON.parse(localStorage.getItem('db_contato'));
 if (!db) {
     db = db_contatos_inicial
 };
+
+
+function Carregar() {
+    const cadastrosArmazenado = JSON.parse(localStorage.getItem('cadastro'));
+    const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"))
+    const dadosUsuarioLogado = cadastrosArmazenado.find(item => item.id == usuarioLogado)
+    document.querySelector('p[name="nomeUser"]').innerHTML = "Bem-vindo(a): " + dadosUsuarioLogado.nome + " | " || "&nbsp";
+    document.getElementById("nome").value = dadosUsuarioLogado.nome || "";
+    document.getElementById("dtaNasc").value = dadosUsuarioLogado.dtaNasc || "&nbsp"
+    const radio = document.getElementById(dadosUsuarioLogado.gender)
+    radio.checked = true;
+    document.getElementById("endereco").value = dadosUsuarioLogado.endereco || "&nbsp"
+    document.getElementById("tel").value = dadosUsuarioLogado.tel || "&nbsp"
+    document.getElementById("cidade").value = dadosUsuarioLogado.cidade || "&nbsp"
+}
+
+function Salvar() {
+
+    let cadastrosArmazenado = JSON.parse(localStorage.getItem('cadastro'));
+    const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"))
+    const novosDados = {
+        nome: document.getElementById("nome").value,
+        dtaNasc: document.getElementById("dtaNasc").value,
+        gender: document.querySelector('input[name="gender"]:checked').value,
+        endereco: document.getElementById("endereco").value,
+        tel: document.getElementById("tel").value,
+        cidade: document.getElementById("cidade").value
+    }
+    cadastrosArmazenado.forEach(item => {
+
+        if (item.id == usuarioLogado) {
+            Object.assign(item, novosDados)
+        }
+    });
+
+    console.log(cadastrosArmazenado[0])
+    localStorage.setItem('cadastro', JSON.stringify(cadastrosArmazenado));
+}
+
+
+function salvarPet() {
+    let cadastrosArmazenado = JSON.parse(localStorage.getItem('cadastro'));
+    const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"))
+    const novosDados = {
+        nomePet: document.getElementById("nomePet").value,
+        racas: document.getElementById("racas").value,
+        dtaNascPet: document.getElementById("dtaNascPet").value,
+        genderPet: document.querySelector('input[name="genderPet"]:checked').value,
+        localVive: document.getElementById("localVive").value,
+        vacinas: document.getElementById("vacinas").value,
+        porte: document.querySelector('input[name="porte"]:checked').value,
+        comportamento: document.getElementById("comportamento").value
+    }
+    cadastrosArmazenado.forEach(item => {
+
+        if (item.id == usuarioLogado) {
+            Object.assign(item, novosDados)
+        }
+    });
+
+
+    localStorage.setItem('cadastro', JSON.stringify(cadastrosArmazenado));
+
+
+}
+
+function meuPet() {
+    const cadastrosArmazenado = JSON.parse(localStorage.getItem('cadastro'));
+    const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"))
+    const dadosUsuarioLogado = cadastrosArmazenado.find(item => item.id == usuarioLogado)
+    console.log(dadosUsuarioLogado)
+    document.getElementById("nomePet").value = dadosUsuarioLogado.nomePet || ""
+    const radio = document.getElementById(dadosUsuarioLogado.genderPet)
+    radio.checked = true;
+    const radioPorte = document.getElementById(dadosUsuarioLogado.porte)
+    radioPorte.checked = true;
+    document.getElementById("racas").value = dadosUsuarioLogado.racas || "&nbsp"
+    document.getElementById("dtaNascPet").value = dadosUsuarioLogado.dtaNascPet || "&nbsp"            
+    document.getElementById("localVive").value = dadosUsuarioLogado.localVive || "&nbsp"
+    document.getElementById("vacinas").value = dadosUsuarioLogado.vacinas || "&nbsp"            
+    document.getElementById("comportamento").value = dadosUsuarioLogado.comportamento || "&nbsp"
+}             
